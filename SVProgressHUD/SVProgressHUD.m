6bcf56dc84af5bd1ab134b530c785c197982828a
trungnguyen1791 <trungnguyen.1791@gmail.com>
2024-06-03 16:32:36 +0700

    Change status messsage's frame

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -417,7 +417,7 @@ static const CGFloat SVProgressHUDLabelSpacing = 8.0f;
     CGFloat labelWidth = 0.0f;
     
     if(self.statusLabel.text) {
-        CGSize constraintSize = CGSizeMake(200.0f, 300.0f);
+        CGSize constraintSize = CGSizeMake([UIScreen mainScreen].bounds.size.width - 32.0f , 300.0f);
         labelRect = [self.statusLabel.text boundingRectWithSize:constraintSize
                                                         options:(NSStringDrawingOptions)(NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin)
                                                      attributes:@{NSFontAttributeName: self.statusLabel.font}
